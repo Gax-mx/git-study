58c4ae9534277546938c7f7356ebc24a60e07390
txgit <txgit@163.com>
2023-07-26 16:57:09 +0800

    V1.1 -txgit --testDrink

--- study.java
+++ study.java
@@ -4,10 +4,15 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         testEat();
+        testDrink();
     }
 
     public static void testEat(){
         System.out.println("eat apple");
+    }
+
+    public static void testDrink(){
+        System.out.println("drink tea");
     }
 
 }
