7784dc286e3654716a118cbca72b9bcaddae6bad
iceorange <3146403070@qq.com>
2023-07-26 16:04:07 +0800

    V1.0 --eat apple

---
+++ study.java
@@ -0,0 +1,13 @@
+package com.core;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        testEat();
+    }
+
+    public static void testEat(){
+        System.out.println("eat apple");
+    }
+
+}
